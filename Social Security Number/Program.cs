78af4e69cab8d1c003c863d53437c5e390a09ab7
Mustafa <mustafasaidhelal@gmail.com>
2020-08-31 11:34:20 +0200

    Add project files.

---
+++ Social Security Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Social_Security_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Social Security Number Input to Evaluate if Person is Male or Female
+            Console.Write("Enter Your Social Security Number in Format (000000-XXXX): ");
+
+
+            // Variable named socialSecurityNumber with an argument of Console.ReadLine() method.
+
+            string socialSecurityNumber = Console.ReadLine();
+
+            int genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2,1));
+
+            string dateToday = socialSecurityNumber.Substring(0, 6);
+
+
+
+            if (genderNumber % 2 == 0)
+            {
+                Console.WriteLine($"You are a Female at the age of {age}.");
+            }
+            else
+            {
+                Console.WriteLine($"You are a Male at the age of {age}");
+            }
+        }
+    }
+}
